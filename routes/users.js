8f4e20bf045ffba5113dbbd782979e78cca53f57
antanim <abdurnouutanim11@gmail.com>
2022-04-26 23:12:58 +0600

    added get user's all posts in routes/posts.js && edited get a user routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -41,10 +41,14 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-// get  a user
-router.get("/:id", async (req, res) => {
+// get a user
+router.get("/", async (req, res) => {
+  const userId = req.query.userId;
+  const username = req.query.username;
   try {
-    const user = await User.findById(req.params.id);
+    const user = userId
+      ? await User.findById(userId)
+      : await User.findOne({ username });
     const { password, createdAt, updatedAt, ...other } = user._doc;
     res.status(200).json(other);
   } catch (err) {
